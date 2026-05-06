4ab4264fb3e0b7475b28f9923931604487ef72ae
akaameet <raiamit078@gmail.com>
2026-05-06 14:43:09 +0545

    chore: register user and auth routes in app configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import authRoutes from "../src/routes/auth.route";
+import userRoutes from "../src/routes/user.route";
+import cookieParser from "cookie-parser";
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 export default app;
