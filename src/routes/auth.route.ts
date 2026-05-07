7a5700bd1844ba45243c168e2a17919d5fddd665
akaameet <raiamit078@gmail.com>
2026-05-07 22:34:30 +0545

    feat(routes): add authentication and user routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,23 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller";
+import {
+  registerUser,
+  loginUser,
+  verifyEmail,
+  forgotPassword,
+  logoutUser,
+  refreshToken,
+  logoutAll,
+  resetPassword,
+} from "../controllers/auth.controller";
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.get("/login", loginUser);
+router.post("/verify-email", verifyEmail);
+router.post("/login", loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.post("/refresh-token", refreshToken);
+router.post("/logout", logoutUser);
+router.post("/logout-All", logoutAll);
 
 export default router;
