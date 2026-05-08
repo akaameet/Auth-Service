72a762f5401b60acdb25ebf8acbb7338f45db6a7
akaameet <raiamit078@gmail.com>
2026-05-08 23:04:35 +0545

    feat: add Redis configuration setup

---
+++ src/config/redis.config.ts
@@ -0,0 +1,20 @@
+import { createClient } from "redis";
+
+const redisClient = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+redisClient.on("error", (err) => {
+  console.log("Redis Error:", err);
+});
+
+redisClient.on("connect", () => {
+  console.log("Redis Connected");
+});
+
+export default redisClient;
