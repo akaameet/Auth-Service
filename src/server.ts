c28ee8d94b6089adc0184aa615040fb34acdfb8c
akaameet <raiamit078@gmail.com>
2026-05-08 23:06:21 +0545

    feat: integrate Redis client connection in server bootstrap

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
 import connectDB from "./db/db";
+import redisClient from "./config/redis.config";
 
 connectDB();
+redisClient.connect();
 
 app.listen(3000, () => {
   console.log("Server is running in port 3000");
